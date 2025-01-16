ca971edceb8198f3f7ebb6942b4f78e2c4ebd24f
Zhilong-Github <git@dimensiondata.com>
2025-01-16 12:49:43 +0000

    Publish - Auto Publish

---
+++ browser/timeline.component-4ANIPE4X.js
@@ -0,0 +1,1 @@
+import{Aa as i,Ba as o,Ga as m,ra as n}from"./chunk-XBYLTWYY.js";var l=(()=>{class e{static{this.\u0275fac=function(t){return new(t||e)}}static{this.\u0275cmp=n({type:e,selectors:[["app-timeline"]],decls:2,vars:0,template:function(t,r){t&1&&(i(0,"p"),m(1,"timeline works!"),o())},encapsulation:2})}}return e})();export{l as TimelineComponent};
